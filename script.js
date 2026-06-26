7ea10fd423f5cc43e4e6f28144d3ef66499818f3
Fardosa <your-actual-email@gmail.com>
2026-06-26 21:01:42 +0300

    update the comments on the pages

--- script.js
+++ script.js
@@ -13,12 +13,29 @@ document.addEventListener('DOMContentLoaded', function() {
             
             if (name.trim() === '') {
                 errorMessage += 'Please enter your name.<br>';
+
+                const fullName = document.getElementById('fullName').value.trim();
+            const namePattern = /^[a-zA-Z\s']+$/; // Regular expression for validating full name (letters, spaces, and apostrophes)
+            if (fullName === '') { // Check if the full name is empty
+                document.getElementById('fullNameError').textContent = 'Full Name is required.';
+                isValid = false;
+            }else if(fullName.length < 3){ // Check if the full name is less than 3 characters
+                document.getElementById('fullNameError').textContent = 'Full Name must be at least 3 characters long.';
+                isValid = false;
+            }else if(!namePattern.test(fullName)){ // Check if the full name contains only letters, spaces, and apostrophes
+                document.getElementById('fullNameError').textContent = 'Full Name can only contain letters, spaces, and apostrophes.';
+                isValid = false;
+            }
             }
             
             if (email.trim() === '') {
                 errorMessage += 'Please enter your email.<br>';
             } else if (!email.includes('@') || !email.includes('.')) {
                 errorMessage += 'Please enter a valid email address.<br>';
+
+
+                 
+
             }
             
             if (message.trim() === '') {
